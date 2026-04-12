ec190d9d21b348c8bd5ed8839c4fd36590437557
prynsu445 <priyanshusharma0142@gmail.com>
2026-04-12 23:42:27 +0530

    arrray codechef part2

--- Array/BobDistance.java
+++ Array/BobDistance.java
@@ -6,31 +6,24 @@ public class BobDistance {
 
         Scanner scanner = new Scanner(System.in);
 
-        int t = scanner.nextInt();   // number of test cases
+        int t = scanner.nextInt();
 
         while (t-- > 0) {
 
-            int n = scanner.nextInt(); // number of days
+            int n = scanner.nextInt();
 
-            int[] a = new int[n]; // Alice distances
-            int[] b = new int[n]; // Bob distances
+            int[] a = new int[n];
 
-            // input Alice distances
             for (int i = 0; i < n; i++) {
                 a[i] = scanner.nextInt();
             }
 
-            // input Bob distances
-            for (int i = 0; i < n; i++) {
-                b[i] = scanner.nextInt();
-            }
-
             int count = 0;
 
-            // check happiness condition
             for (int i = 0; i < n; i++) {
+                int b = scanner.nextInt();
 
-                if (a[i] <= 2 * b[i] && b[i] <= 2 * a[i]) {
+                if (a[i] <= 2 * b && b <= 2 * a[i]) {
                     count++;
                 }
             }
