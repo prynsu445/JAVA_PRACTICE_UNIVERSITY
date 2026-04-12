5a7bd408b61bfead82f1a253e820e9fb42907e8c
prynsu445 <priyanshusharma0142@gmail.com>
2026-04-12 23:44:25 +0530

    chefcode array part-3

--- Array/BobDistance.java
+++ Array/BobDistance.java
@@ -13,17 +13,20 @@ public class BobDistance {
             int n = scanner.nextInt();
 
             int[] a = new int[n];
+            int[] b = new int[n];  // Create array for b values
 
             for (int i = 0; i < n; i++) {
                 a[i] = scanner.nextInt();
+            }
+            
+            for (int i = 0; i < n; i++) {  // Read b values separately
+                b[i] = scanner.nextInt();
             }
 
             int count = 0;
 
             for (int i = 0; i < n; i++) {
-                int b = scanner.nextInt();
-
-                if (a[i] <= 2 * b && b <= 2 * a[i]) {
+                if (a[i] <= 2 * b[i] && b[i] <= 2 * a[i]) {
                     count++;
                 }
             }
