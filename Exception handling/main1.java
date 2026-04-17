fc21474ba9a4a2b17c4d1ef7cfb667d87ec30e1e
prynsu445 <priyanshusharma0142@gmail.com>
2026-04-17 22:04:12 +0530

    modifier of suspend

--- Exception handling/main1.java
+++ Exception handling/main1.java
@@ -1,40 +1,90 @@
 import java.io.*;
+
 class MyThread extends Thread {
-    public void run()
-    {
+
+    private boolean paused = false;
+
+    public void run() {
+
         for (int i = 0; i < 10; i++) {
-            System.out.println(" Running thread : " + i);
+
+            System.out.println("Running thread : " + i);
+
             try {
                 Thread.sleep(1000);
+
+                synchronized (this) {
+
+                    while (paused) {
+                        wait();   // thread wait until resume
+                    }
+
+                }
+
+            } catch (InterruptedException e) {
+
+                System.out.println(e);
+
             }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
         }
+
     }
+
+    // suspend method
+    public void pauseThread() {
+
+        paused = true;
+
+    }
+
+    // resume method
+    public synchronized void resumeThread() {
+
+        paused = false;
+
+        notify();   // wake up thread
+
+    }
+
 }
-class main1 {
-    public static void main(String[] args)
-    {
+
+class GFG {
+
+    public static void main(String[] args) {
+
         MyThread t1 = new MyThread();
+
         t1.start();
+
         try {
+
             Thread.sleep(3000);
+
+        } catch (InterruptedException e) {
+
+            System.out.println(e);
+
         }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // suspend the execution of the thread
-        t1.suspend();
-        System.out.println("Suspended thread ");
+
+        t1.pauseThread();
+
+        System.out.println("Thread Suspended");
+
         try {
+
             Thread.sleep(3000);
+
+        } catch (InterruptedException e) {
+
+            System.out.println(e);
+
         }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // resume the execution of the thread
-        t1.resume();
-        System.out.println("Resumed thread");
+
+        t1.resumeThread();
+
+        System.out.println("Thread Resumed");
+
     }
+
 }
