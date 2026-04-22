505f4494a827f21e00bbe9ca50214f3e89b9ee68
prynsu445 <priyanshusharma0142@gmail.com>
2026-04-23 03:19:08 +0530

    matching leetcode 3

--- leetcode/isMatch.java
+++ leetcode/isMatch.java
@@ -6,10 +6,8 @@ int n = p.length();
 ```
     boolean[][] dp = new boolean[m + 1][n + 1];
 
-    // Base case
     dp[0][0] = true;
 
-    // Handle patterns like a*, a*b*, a*b*c*
     for (int j = 2; j <= n; j++) {
         if (p.charAt(j - 1) == '*') {
             dp[0][j] = dp[0][j - 2];
@@ -26,10 +24,9 @@ int n = p.length();
                 dp[i][j] = dp[i - 1][j - 1];
             } 
             else if (pc == '*' && j >= 2) {
-                // zero occurrence
+
                 dp[i][j] = dp[i][j - 2];
 
-                // one or more occurrence
                 char prev = p.charAt(j - 2);
                 if (prev == '.' || prev == sc) {
                     dp[i][j] = dp[i][j] || dp[i - 1][j];
