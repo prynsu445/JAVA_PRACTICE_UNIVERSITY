5b8e82610e0a19993eed429c4649205e0a751248
prynsu445 <priyanshusharma0142@gmail.com>
2026-04-23 03:17:26 +0530

    matching leetcode 2

--- leetcode/isMatch.java
+++ leetcode/isMatch.java
@@ -4,33 +4,28 @@ int m = s.length();
 int n = p.length();
 
 ```
-    // dp[i][j] = true if s[0..i-1] matches p[0..j-1]
     boolean[][] dp = new boolean[m + 1][n + 1];
 
-    // Base case: empty string & empty pattern
+    // Base case
     dp[0][0] = true;
 
-    // Handle patterns like a*, a*b*, etc. for empty string
+    // Handle patterns like a*, a*b*, a*b*c*
     for (int j = 2; j <= n; j++) {
         if (p.charAt(j - 1) == '*') {
             dp[0][j] = dp[0][j - 2];
         }
     }
 
-    // Fill DP table
     for (int i = 1; i <= m; i++) {
         for (int j = 1; j <= n; j++) {
 
             char sc = s.charAt(i - 1);
             char pc = p.charAt(j - 1);
 
-            // Case 1: match or '.'
             if (pc == '.' || pc == sc) {
                 dp[i][j] = dp[i - 1][j - 1];
-            }
-
-            // Case 2: '*'
-            else if (pc == '*') {
+            } 
+            else if (pc == '*' && j >= 2) {
                 // zero occurrence
                 dp[i][j] = dp[i][j - 2];
 
