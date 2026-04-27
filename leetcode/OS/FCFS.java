801c27352603c936d7167652908331e93a9a1ca6
prynsu445 <priyanshusharma0142@gmail.com>
2026-04-27 19:44:00 +0530

    fcfs modify 2

--- leetcode/OS/FCFS.java
+++ leetcode/OS/FCFS.java
@@ -7,31 +7,46 @@ class FCFS {
         System.out.print("Enter number of processes: ");
         int n = sc.nextInt();
 
-        int[] bt = new int[n]; // Burst Time
-        int[] wt = new int[n]; // Waiting Time
-        int[] tat = new int[n]; // Turnaround Time
+        int[] bt = new int[n];
+        int[] wt = new int[n];
+        int[] tat = new int[n];
 
         // Input burst time
         for (int i = 0; i < n; i++) {
-            System.out.print("Enter burst time for P" + (i + 1) + ": ");
+            System.out.print("Enter BT for P" + (i+1) + ": ");
             bt[i] = sc.nextInt();
         }
 
-        // Calculate waiting time
-        wt[0] = 0;
-        for (int i = 1; i < n; i++) {
-            wt[i] = wt[i - 1] + bt[i - 1];
+        // Ready Queue (FCFS = input order)
+        System.out.print("\nReady Queue: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("P" + (i+1) + " ");
         }
 
-        // Calculate turnaround time
+        // Waiting time
+        wt[0] = 0;
+        for (int i = 1; i < n; i++) {
+            wt[i] = wt[i-1] + bt[i-1];
+        }
+
+        // Turnaround time
         for (int i = 0; i < n; i++) {
             tat[i] = wt[i] + bt[i];
         }
 
-        // Display
-        System.out.println("\nProcess\tBT\tWT\tTAT");
+        // Gantt Chart
+        System.out.println("\n\nGantt Chart:");
+        System.out.print("0 ");
+        int time = 0;
         for (int i = 0; i < n; i++) {
-            System.out.println("P" + (i + 1) + "\t" + bt[i] + "\t" + wt[i] + "\t" + tat[i]);
+            time += bt[i];
+            System.out.print("| P" + (i+1) + " | " + time + " ");
+        }
+
+        // Table
+        System.out.println("\n\nProcess\tBT\tWT\tTAT");
+        for (int i = 0; i < n; i++) {
+            System.out.println("P"+(i+1)+"\t"+bt[i]+"\t"+wt[i]+"\t"+tat[i]);
         }
 
         sc.close();
